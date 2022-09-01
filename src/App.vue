b44801d11415e165a469ab74e91f7789f49902c3
Mohamed <nrookman_27@yahoo.com>
2022-09-01 22:55:24 +0200

    Add main design

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,46 @@
-<template></template>
+<template>
+    <learning-survey @survey-submit="storeSurvey"></learning-survey>
+    <user-experiences :results="savedSurveyResults"></user-experiences>
+</template>
 
-<script></script>
+<script>
+import LearningSurvey from "./components/survey/LearningSurvey.vue";
+import UserExperiences from "./components/survey/UserExperiences.vue";
 
-<style></style>
+export default {
+    components: {
+        LearningSurvey,
+        UserExperiences,
+    },
+    data() {
+        return {
+            savedSurveyResults: [],
+        };
+    },
+    methods: {
+        storeSurvey(surveyData) {
+            const surveyResult = {
+                name: surveyData.userName,
+                rating: surveyData.rating,
+                id: new Date().toISOString(),
+            };
+            this.savedSurveyResults.unshift(surveyResult);
+            console.log(surveyResult);
+        },
+    },
+};
+</script>
+
+<style>
+* {
+    box-sizing: border-box;
+}
+
+html {
+    font-family: sans-serif;
+}
+
+body {
+    margin: 0;
+}
+</style>
